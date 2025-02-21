7ac2057652f5982adfa789ce371de378928cdf36
vphilavong <vphilavong0@gmail.com>
2025-02-21 15:27:54 -0600

    added docker

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework.response import Response, Redirect
+from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from spotify.models import SpotifyToken
 from .serializer import ItemSerializer
